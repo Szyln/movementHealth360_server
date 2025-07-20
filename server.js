64b05781c7d61ed06b70c099cb367a27c57a137c
Szyln <szyen.lin@gmail.com>
2025-07-20 22:20:43 +0800

    cors

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 server.use(router)
 server.listen(process.env.PORT || 3004, () => {
   console.log('JSON Server is running')
